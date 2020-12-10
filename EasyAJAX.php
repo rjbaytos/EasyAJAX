a5f79b7c55a95dca4ba779f8167f65aaeb58dce6
Rhyan <38738366+rjbaytos@users.noreply.github.com>
2020-12-11 02:22:58 +0800

    Add files via upload

--- EasyAJAX.php
+++ EasyAJAX.php
@@ -112,43 +112,77 @@ namespace EasyAJAX {
 			return <<<ajaxscript
 // EASY AJAX LIBRARY BY RHYAN JILL
 
+var	autoBindAJAXLinks = false,
+	AJAXLinkTarget = 'body',
+	AJAXLinkSource = 'a',
+	AJAXLinkInsert = '',
+	AJAXLinkAttrib = 'href',
+	autoBindAJAXForms = false,
+	AJAXFormTarget = 'body',
+	AJAXFormSource = 'form',
+	AJAXFormAppend = '',
+	AJAXFormPushData = null,
+	AJAXFormPushAppendedData = false;
+
+// TURN A FORM INTO AN AJAX FORM AUTOMATICALLY
+function autoAJAXForm( target, form, appendedData, pushData, pushAppendedData ) {
+	autoBindAJAXForms = true;
+	AJAXFormTarget = (typeof target !== 'undefined') ? target : 'body';
+	AJAXFormSource = (typeof form !== 'undefined') ? form : 'form';
+	AJAXFormAppend = (typeof appendedData !== 'undefined') ? appendedData : '';
+	AJAXFormPushData = (pushData !== true && pushData !== false) ? null : pushData;
+	AJAXFormPushAppendedData = (pushAppendedData !== true) ? false : pushAppendedData;
+	AJAXForm ( AJAXFormTarget, AJAXFormSource, AJAXFormAppend, AJAXFormPushData, AJAXFormPushAppendedData );
+}
+
+// TURN A SOURCE ELEMENT INTO AN AJAX LINK AUTOMATICALLY
+function autoAJAXLink ( target, source, insert, attrib ){
+	autoBindAJAXLinks = true;
+	AJAXLinkTarget = ( typeof target !== 'undefined' ) ? target : 'body';				// AJAX OUTPUT
+	AJAXLinkSource = ( typeof source !== 'undefined' ) ? source : 'a';					// ELEMENT PAGE ADDRESS
+	AJAXLinkInsert = ( typeof insert !== 'undefined' ) ? insert : '';					// ADDITIONAL DATA TO ADD TO REQUEST
+	AJAXLinkAttrib = ( typeof attrib !== 'undefined' ) ? attrib.toLowerCase() : 'href';	// ATTRIBUTE NAME OF PAGE ADDRESS
+	AJAXLink ( AJAXLinkTarget, AJAXLinkSource, AJAXLinkInsert, AJAXLinkAttrib );
+}
+
 // TURN A SOURCE ELEMENT INTO AN AJAX LINK
-function AJAXLink ( target, source, insert, attrib )
-{
+function AJAXLink ( target, source, insert, attrib ){
 	target = ( typeof target !== 'undefined' ) ? target : 'body';				// AJAX OUTPUT
 	source = ( typeof source !== 'undefined' ) ? source : 'a';					// ELEMENT PAGE ADDRESS
 	insert = ( typeof insert !== 'undefined' ) ? insert : '';					// ADDITIONAL DATA TO ADD TO THE REQUEST STRING
 	attrib = ( typeof attrib !== 'undefined' ) ? attrib.toLowerCase() : 'href';	// ATTRIBUTE NAME OF PAGE ADDRESS
 	defaultTarget = target;
-	$(function()
-	{
-		$(source).click(function()
-		{
+	$(function(){
+		$(source).click(function(){
 			var url = $(this).attr(attrib);
-			if ( $(this).attr('data-nopush') === undefined ) {
-				history.pushState({}, '', url);
+			if (	$(this).attr('target') !== undefined &&
+					$(this).attr('target').toLowerCase() == '_top' ){
+				window.open(url, '_self');
+			} else if (	$(this).attr('target') !== undefined &&
+					$(this).attr('target').toLowerCase() == '_blank' ){
+				window.open(url, '_blank');
+			} else {
+				if ( $(this).attr('data-nopush') === undefined ) {
+					history.pushState({}, '', url);
+				}
+				if (	$(this).attr('target') !== undefined &&
+						$(this).attr('target').toLowerCase() !== '_blank' ){
+					target = $(this).attr('target');
+				} else if ( $(this).attr('target') === undefined ){
+					target = defaultTarget;
+				}
+				
+				getpage(url, target, insert, function(data){
+					target = defaultTarget;
+				});
 			}
-			if	(	$(this).attr('target') !== undefined &&
-					$(this).attr('target').toLowerCase() !== '_blank'
-				) {
-				target = $(this).attr('target');
-			}
-			else if ( $(this).attr('target') === undefined ) {
-				target = defaultTarget;
-			}
-			
-			getpage(url, target, insert, function(data){
-				target = defaultTarget;
-			});
-			
 			return false;
 		});
 	});
 }
 
 // LOAD A WEBPAGE INTO A TARGET ELEMENT
-function getpage ( url, target, insert, anonfn )
-{
+function getpage ( url, target, insert, anonfn ){
 	if ( typeof url === 'undefined' ) { return false; }
 	if ( url === '' || url === './' && insert !== '' ) { url = '?' };
 	target = ( typeof target !== 'undefined' ) ? target : 'body';
@@ -191,6 +225,17 @@ function getpage ( url, target, insert, anonfn )
 							});
 						}
 						anonfn(data);
+						if ( autoBindAJAXLinks ){
+							AJAXLink ( AJAXLinkTarget, AJAXLinkSource, AJAXLinkInsert, AJAXLinkAttrib );
+						}
+						if ( autoBindAJAXForms ){
+							AJAXForm (	AJAXFormTarget,
+										AJAXFormSource,
+										AJAXFormAppend,
+										AJAXFormPushData,
+										AJAXFormPushAppendedData
+									);
+						}
 					});
 				});
 			}
@@ -199,8 +244,7 @@ function getpage ( url, target, insert, anonfn )
 }
 
 // DISPLAY LOADING INDICATOR
-function loader ( targetElement, doAfter )
-{
+function loader ( targetElement, doAfter ){
 	if ( typeof targetElement === 'undefined' ) return;
 	$(targetElement).stop().promise().done(function(){
 		var w =	window.innerWidth
@@ -218,8 +262,7 @@ function loader ( targetElement, doAfter )
 			dimensions = w / divisor;
 		}
 		thickness = dimensions / 5;
-		$(this).stop().fadeOut(100, function()
-		{
+		$(this).stop().fadeOut(100, function(){
 			if ( $(this).attr('data-display') !== undefined ) {
 				if ( $(this).attr('data-display') == '' ) {
 					$(this).css({'display':'grid'});
@@ -241,8 +284,7 @@ function loader ( targetElement, doAfter )
 }
 
 // PROGRAMMATICALLY LOAD URL
-function loadURL ( destURL, targetObj, urlAdditions, trimAfter )
-{
+function loadURL ( destURL, targetObj, urlAdditions, trimAfter ){
 	if ( typeof urlAdditions === 'undefined' ) urlAdditions = '';
 	if ( typeof targetObj === 'undefined' ) targetObj = 'body';
 	if ( destURL === '' || destURL === './' && urlAdditions !== '' ) { destURL = '?'; }
@@ -261,8 +303,7 @@ function loadURL ( destURL, targetObj, urlAdditions, trimAfter )
 }
 
 // LOAD A WEBPAGE INTO A TARGET ELEMENT - CONVERT IF RESULT IS JSON
-function loadList ( url, target, anonfn )
-{
+function loadList ( url, target, anonfn ){
 	if ( typeof url === 'undefined' ) return false;
 	target = ( typeof target !== 'undefined' ) ? target : 'body';
 	anonfn = ( typeof anonfn !== 'undefined' ) ? anonfn : function(data){
@@ -284,8 +325,7 @@ function loadList ( url, target, anonfn )
 }
 
 // TURN PARSED JSON INTO HTML
-function implode_recursive ( data, separator, counter )
-{
+function implode_recursive ( data, separator, counter ){
 	var HTMLContent = '';
 	var separatorIsArray = Array.isArray ( separator );
 	if ( typeof counter != 'number' ) counter = 0;
@@ -313,26 +353,25 @@ function implode_recursive ( data, separator, counter )
 }
 
 // MAKE FORMS ASYNCHRONOUS
-function AJAXForm ( target, form, appendedData, pushData, pushAppendedData )
-{
+function AJAXForm ( target, form, appendedData, pushData, pushAppendedData ){
 	if (typeof target === 'undefined') target = 'body';
 	if (typeof form === 'undefined') form = 'form';
 	if (typeof appendedData === 'undefined') appendedData = '';
 	if (pushData !== true && pushData !== false) pushData = null;
 	if (pushAppendedData !== true) pushAppendedData = false;
 	
-	$(function()
-	{
-		$(form).submit(function (event)
-		{
+	$(function(){
+		$(form).submit(function (event){
 			//PREVENT DEFAULT FORM SUBMISSION
 			event.preventDefault();
 			
+			// GET FORM DATA BEFORE ERASING THE FORM
+			var formMethod = $(form).attr('method').toLowerCase(),
+				formAction = $(form).attr('action'),
+				formData = $(form).serialize();
+			
 			//DISPLAY LOADER & SEND DATA
 			loader(target,function(){
-				var formMethod = $(form).attr('method').toLowerCase(),
-					formAction = $(form).attr('action'),
-					formData = $(form).serialize();
 				if ( formAction == 'undefined' ) { formAction = window.location.href.split("?")[0]; }
 				$.ajax(
 				{
@@ -354,6 +393,100 @@ function AJAXForm ( target, form, appendedData, pushData, pushAppendedData )
 			});
 		});
 	});
+}
+
+class slideshow {
+	constructor(slides, dots, timer, slideIndex){
+		// bind 'this' for recursion to work
+		this.playSlides = this.playSlides.bind(this);
+		if ( typeof slides === 'string' ){
+			if ( $(slides + ' > div.mySlides').length ){
+				this.slides = $(slides + ' > div.mySlides');
+			} else if ( $(slides).length ){
+				this.slides = $(slides);
+			}
+		}
+		
+		if ( typeof dots === 'string' && $(dots).length ){
+			this.dots = $(dots);
+		} else if (	typeof slides === 'string' &&
+					$(slides.split('>')[0].trim() + ' > div.slidecontroller > span.dot').length ){
+			this.dots = $(slides.split('>')[0].trim() + ' > div.slidecontroller > span.dot');
+		}
+		
+		this.timer = (timer !== undefined) ? timer : 5000;
+		this.slideIndex = (slideIndex !== undefined) ? slideIndex : 1;
+		var myself = this;
+		$(slides.split('>')[0].trim() + ' > a.prev').click(function(){
+			myself.plusSlides(-1);
+		});
+		$(slides.split('>')[0].trim() + ' > a.next').click(function(){
+			myself.plusSlides(1);
+		});
+		$(slides.split('>')[0].trim() + ' > div.slidecontroller > span.dot').click(function(){
+			myself.currentSlide($(this).attr('value'));
+		});
+		myself.displaySlide();
+	}
+	
+	// Next/previous controls
+	plusSlides(n) {
+		if ( this.slides === undefined ) return 'fail';
+		this.displaySlide(this.slideIndex += n);
+	}
+	
+	// Thumbnail image controls
+	currentSlide(n) {
+		if ( this.slides === undefined ) return 'fail';
+		this.displaySlide(this.slideIndex = n);
+	}
+	
+	displaySlide(n) {
+		if ( this.slides === undefined ) return 'fail';
+		if (typeof n === undefined) n = this.slideIndex;
+		if (typeof this.activeSlide !== undefined) clearTimeout(this.activeSlide);
+		var i;
+		
+		var slides = this.slides;
+		if (n > slides.length) {this.slideIndex = 1}
+		if (n < 1) {this.slideIndex = slides.length}
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none';
+		}
+		slides[this.slideIndex-1].style.display = 'block';
+		
+		if ( this.dots !== undefined ){
+			var dots = this.dots;
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(' active', '');
+			}
+			dots[this.slideIndex-1].className += ' active';
+		}
+		
+		this.activeSlide = setTimeout(this.playSlides, this.timer);
+	}
+	
+	playSlides() {
+		if ( this.slides === undefined ) return 'fail';
+		var i;
+		var slides = this.slides;
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none';
+		}
+		if ( this.dots !== undefined ){
+			var dots = this.dots;
+			for (i = 0; i < dots.length; i++) {
+				dots[i].className = dots[i].className.replace(' active', '');
+			}
+		}
+		this.slideIndex++;
+		if (this.slideIndex > slides.length) {this.slideIndex = 1}
+		slides[this.slideIndex-1].style.display = 'block';
+		if ( this.dots !== undefined ){
+			dots[this.slideIndex-1].className += ' active';
+		}
+		this.activeSlide = setTimeout(this.playSlides, this.timer);
+	}
 }
 ajaxscript;
 		}
